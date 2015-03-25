21e9508d8df016111af96b627388494dce70e1c1
DOLLOR <dolloyo@gmail.com>
2015-03-25 13:05:14 +0800

    First submit|初次提交
    最初的文件

---
+++ src/dlb.js
@@ -0,0 +1,41 @@
+//dont-look-back
+//by DOLLOR
+~(function(global){
+	"use strict";
+	var obj,init;
+	obj={};
+	init=function(){
+		var lastHash=global.location.hash || "#####";
+		var preventBack=function(){
+			if(global.location.hash!==lastHash){
+				//global.location.href=""+global.location.pathname;
+				setTimeout(function(){
+					global.location.hash=lastHash;
+				},100)
+			}
+		};
+		//global.open(lastHash,"_self");
+		if(history && history.pushState){
+			history.pushState({},document.title,lastHash+"##");
+			history.pushState({},document.title,"###");
+		}
+		else{
+			global.location.hash=lastHash+"##";
+			global.location.hash="###";
+		}
+		preventBack();
+		global.onhashchange=preventBack;
+		obj.setHash=function(newhash){
+			lastHash=newhash;
+			global.location.hash="##";
+		};
+		obj.getHash=function(){
+			return global.location.hash;
+		};
+	};
+	//output
+	if(!global.dlb){
+		init();
+		global.dlb=global.dlb || obj;
+	}
+})(window);
