65d860dd9bea3eaa059a72b0ac0c711e70d633e2
DOLLOR <dolloyo@gmail.com>
2015-12-07 12:38:18 +0800

    Update dlb.js

--- src/dlb.js
+++ src/dlb.js
@@ -4,15 +4,53 @@
 	"use strict";
 	var obj,init;
 	obj={};
-	obj.enable = true
+	obj.enable=true
 	init=function(){
-		var lastHash=global.location.hash || "#";
-		var preventBack=function(){
+		//封装改变hash方法
+		obj.changeHashTo=function(newhash){
+			newhash="#"+newhash;
+			if(history && history.pushState){
+				if(newhash!=="#"){
+					history.pushState({},document.title,newhash);
+				}
+				else{
+					history.pushState({},document.title,
+						location.href.split("#")[0]
+					);
+				}
+			}
+			else{
+				global.location.hash=newhash
+			}
+		};
+		//加倍改变
+		obj.changeManyTimes=function(newhash){
+			obj.changeHashTo(newhash+"#3");
+			obj.changeHashTo(newhash+"#2");
+			obj.changeHashTo(newhash+"#1");
+			obj.changeHashTo(newhash);
+		};
+		//保存旧的并设置新的
+		obj.setHash=function(newhash){
+			obj.lastHash=newhash;
+			obj.changeManyTimes(obj.lastHash);
+		};
+		obj.getHash=function(){
+			var hash=global.location.hash;
+			if(hash[0]==="#"){
+				hash=hash.slice(1);
+			}
+			return hash;
+		};
+		//响应并阻止后退
+		obj.preventBack=function(){
+			//获取改变后的hash
 			var newhash = obj.getHash();
-			if(obj.enable && newhash!==lastHash){
-				if(newhash===lastHash+"#1"){
+			//检查hash是否变了
+			if(obj.enable && newhash!==obj.lastHash){
+				//检查是后退还是前进
+				if(newhash===obj.lastHash+"#1"){
 					setTimeout(function(){
-						//global.location.hash=lastHash;
 						history.go(1);
 					},50)
 				}
@@ -21,30 +59,24 @@
 				}
 			}
 		};
-		if(history && history.pushState){
-			history.pushState({},document.title,lastHash+"#3");
-			history.pushState({},document.title,lastHash+"#2");
-			history.pushState({},document.title,lastHash+"#1");
-			history.pushState({},document.title,lastHash);
+		//当前想保持的hash
+		obj.lastHash=obj.getHash();
+
+		obj.changeManyTimes(obj.lastHash);
+		obj.preventBack();
+		if(global.addEventListener){
+			global.addEventListener("hashchange",function(ev){
+				obj.preventBack();
+			});
+		}
+		else if(global.attachEvent){
+			global.attachEvent("onhashchange",function(ev){
+				obj.preventBack();
+			});
 		}
 		else{
-			global.location.hash=lastHash+"#3";
-			global.location.hash=lastHash+"#2";
-			global.location.hash=lastHash+"#1";
-			global.location.hash=lastHash;
+			//global.onhashchange=obj.preventBack;
 		}
-		obj.setHash=function(newhash){
-			lastHash=newhash;
-			global.location.hash=lastHash+"#3";
-			global.location.hash=lastHash+"#2";
-			global.location.hash=lastHash+"#1";
-			global.location.hash=lastHash;
-		};
-		obj.getHash=function(){
-			return global.location.hash || "#";
-		};
-		preventBack();
-		global.onhashchange=preventBack;
 	};
 	//output
 	if(!global.dlb){
